f97275fd5fb5b407b35173b94691f9d01cb462e5
Srikar <srikar.iiit@gmail.com>
2023-08-10 12:38:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -143,13 +143,13 @@ usage_examples:
 data_dictionary:
   featured: 
     database: "db_another_NAME"
-      objects:
-        - schema: "DATA_DICTIONARY_API_SCHEMA_NAME"
-          domain: TABLE
-          name: "TABLE_ANOTHER_NAME"
-        - schema: "DATA_DICTIONARY_API_SCHEMA_NAME"
-          domain: TABLE
-          name: "DATA_DICTIONARY_API_TABLE_NAME"
+    objects:
+      - schema: "DATA_DICTIONARY_API_SCHEMA_NAME"
+        domain: TABLE
+        name: "TABLE_ANOTHER_NAME"
+      - schema: "DATA_DICTIONARY_API_SCHEMA_NAME"
+        domain: TABLE
+        name: "DATA_DICTIONARY_API_TABLE_NAME"
 business_needs:
   - name: "Data Quality and Cleansing"
     description: "Test listing for data cleansing"
