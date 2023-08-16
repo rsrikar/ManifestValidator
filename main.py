cbdaeb9a85d86bdc8fb1e5c71ce10bd4e09067d8
Srikar <srikar.iiit@gmail.com>
2023-08-16 11:47:54 -0700

    using yamale

--- main.py
+++ main.py
@@ -1,136 +1,26 @@
 import streamlit as st
-import yaml
-from jsonschema import validate
+import yamale
 import json
+import yaml
+from yamale import YamaleError
 
-schema = """
-type: object
-properties:
-  title:
-    type: string
-  subtitle:
-    type: string
-  description:
-    type: string
-  custom_contact:
-    type: string
-  profile:
-    type: string
-  terms_of_service:
-    type: object
-    properties:
-      type:
-        enum:
-          - STANDARD
-          - CUSTOM
-          - OFFLINE
-      link:
-        type: string
-  auto_fulfillment:
-    type: object
-    properties:
-      refresh_schedule:
-        type: string
-      refresh_type:
-        enum:
-          - FULL_DATABASE
-          - SUB_DATABASE
-  targets:
-    type: object
-    properties:
-      accounts:
-        type: array
-        items:
-          type: string
-  usge_examples:
-    type: array
-    items:
-      type: object
-      properties:
-        title:
-          type: string
-        description:
-          type: string
-        query:
-          type: string
-  data_dictionary:
-    type: object
-    properties:
-      featured:
-        type: object
-        properties:
-          database:
-            type: string
-            objects:
-              type: array
-              items:
-                type: object
-                properties:
-                  name:
-                    type: string
-                  schema:
-                    type: string
-                  domain:
-                    enum:
-                      - DATABASE
-                      - SCHEMA
-                      - TABLE
-                      - VIEW
-                      - EXTERNAL_TABLE
-                      - MATERIALIZED_VIEW
-                      - DIRECTORY_TABLE
-                      - FUNCTION
-                      - COLUMN
-  business_needs:
-    type: array
-    items:
-      type: object
-      properties:
-        name:
-          enum:
-            - 360-Degree Customer View
-            - Supply Chain
-            - Personalize Customer Experiences
-            - Inventory Management
-            - Accelerating Advertising Revenue
-            - Attribution Analysis
-            - Contact Data Enrichment
-            - Foot Traffic Analytics
-            - Audience Segmentation
-            - Sentiment Analysis
-            - ESG Investment Analysis
-            - Fundamental Analysis
-            - Quantitative Analysis
-            - Risk Analysis
-            - Fraud Remediation
-            - Customer Onboarding
-            - Identity Resolution
-            - Asset Valuation
-            - Economic Impact Analysis
-            - Demand Forecasting
-            - Population Health Management
-            - Real World Data (RWD)
-            - Location Planning
-            - Regulatory Reporting
-            - Subscriber Acquisition and Retention
-            - Life Sciences Commercialization
-            - Patient 360
-            - Blockchain Analysis
-            - Customer Acquisition
-            - Data Quality and Cleansing
-            - Location Data Enrichment
-            - Location Geocoding
-            - Machine Learning
-            - Market Analysis
-            - Pricing Analysis
-            - Audience Activation
-        description:
-          type: string
-"""
+def displayValidateButton(col):
+   col.write('')
+   col.write('')
+   col.write('')
+   col.write('')
+   col.write('')
+   col.write('')
+   col.button('Validate', use_container_width=True)
 
+st. set_page_config(layout="wide")
 
-txt = st.text_area('Manifest validator', '''
-title: "title1"
+st.header("Snowflake marketplace listing manifest validator")
+
+col1, col2, col3 = st.columns([3,1,3])
+
+yamlText = col1.text_area('Enter the manifest to validate', 
+'''title: "title1"
 subtitle: "sub title 1"
 description: "listing description"
 terms_of_service:
@@ -155,15 +45,32 @@ business_needs:
   - name: "Data Quality and Cleansing"
     description: "Test listing for data cleansing"
 ''',
-height=500,
-placeholder="Manifest to validate ...")
+    height=600,
+    placeholder="Manifest to validate ...")
 
 try:
-  validate(yaml.safe_load(txt), yaml.safe_load(schema)) # passes
-  json_string = json.dumps(yaml.safe_load(txt))
-  st.write("Valid manifest")
-  st.json(json_string)
-except Exception as e:
-  st.write("Invalid manifest")
-  json_string = ""
-  st.write(str(e))
+  
+  displayValidateButton(col2)
+
+  # read the schema
+  schema = yamale.make_schema('./listingManifestSchema.yaml')
+  
+  # construct yaml data object to be passed to yamale validate
+  yamlDataObject = yamale.make_data(content=yamlText)
+
+  # validate the yaml data object against the schema
+  yamale.validate(schema, yamlDataObject)
+
+  # update status to say valid manifest
+  col3.write('**Valid manifest**')
+
+  # display the valid yaml as a pretty-printed JSON
+  json_string = json.dumps(yaml.safe_load(yamlText))
+  #col3.json(json_string)
+except YamaleError as e:
+    col3.write('**Validation failed!**')
+    for result in e.results:
+        for error in result.errors:
+            col3.write('\t%s' % error)
+finally:    
+  col3.code(yamlText, language='yaml', line_numbers=True)
